f98916bf6c9db3682774edcd9591327203fdeb50
grooveguang <908749236@qq.com>
2018-01-31 21:05:25 +0800

    1
    q

--- src/test/java/com/grooveguang/tool/ToolApplicationTests.java
+++ src/test/java/com/grooveguang/tool/ToolApplicationTests.java
@@ -1,5 +1,9 @@
 package com.grooveguang.tool;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,4 +17,31 @@ public class ToolApplicationTests {
 	public void contextLoads() {
 	}
 
+	public static void main(String[] args) {
+	
+	    List languages = Arrays.asList("Java", "Scala", "C++", "Haskell", "Lisp");
+	 
+	    System.out.println("Languages which starts with J :");
+	    filter(languages, (str)->((String) str).startsWith("J"));
+	 
+	    System.out.println("Languages which ends with a ");
+	    filter(languages, (str)->((String) str).endsWith("a"));
+	 
+	    System.out.println("Print all languages :");
+	    filter(languages, (str)->true);
+	 
+	    System.out.println("Print no language : ");
+	    filter(languages, (str)->false);
+	 
+	    System.out.println("Print language whose length greater than 4:");
+	    filter(languages, (str)->((String) str).length() > 4);
+	}
+	 
+	public static void filter(List<String> names, Predicate condition) {
+	    for(String name: names)  {
+	        if(condition.test(name)) {
+	            System.out.println(name + " ");
+	        }
+	    }
+	}
 }
